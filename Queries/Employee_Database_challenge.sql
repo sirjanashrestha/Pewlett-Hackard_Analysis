cca0fe44abefc945d90c786b4976887be72caa36
sirjanashrestha <sirjanashrestha869@gmail.com>
2022-08-17 16:32:41 -0400

    Adding Resources folder, QuickDBD Diagram

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,10 +29,14 @@ SELECT *
 FROM Unique_titles
 
 --Retrieve number of employees by most recent job title who are about to retire
-SELECT COUNT(title) "count",title
+SELECT COUNT(title)"count",title
+INTO retiring_titles
 FROM Unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+SELECT *
+FROM retiring_titles
 
 ----DELIVERABLE 2
 
@@ -55,5 +59,29 @@ SELECT *
 FROM Mentorship_Eligibility
 
 
+-- Create a depart_info table to retreive retirement ready employee in respective department
+SELECT ut.emp_no, ut.first_name, ut.last_name,ut.title, d.dept_name
+INTO department_info
+FROM unique_titles as ut
+INNER JOIN dep_employee as de
+ON ut.emp_no=de.emp_no
+INNER JOIN departments as d
+ON de.dept_no=d.dept_no
+ORDER BY ut.emp_no
 
 
+SELECT *
+FROM department_info
+
+-- Retrieve department wise information
+SELECT di.dept_name,count(di.emp_no)
+FROM department_info as di
+GROUP BY di.dept_name
+ORDER BY count(di.emp_no);
+
+-- Retrieve qualified retirement ready employee to mentor next generation of PH
+SELECT ut.title,count(ut.title)
+FROM unique_titles as ut
+WHERE ut.title in ('Senior Engineer','Senior Staff','Manager','Technique Leader')
+GROUP BY ut.title
+ORDER BY ut.title
